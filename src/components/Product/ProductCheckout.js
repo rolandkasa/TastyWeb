8a24b244c64dd809d36376dacc1ae9dfb79ab0fd
Simonesiss <simona.radulea@gmail.com>
2021-05-12 16:41:33 +0300

    Add modify quantity functionality

--- src/components/Product/ProductCheckout.js
+++ src/components/Product/ProductCheckout.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { FaMinusCircle, FaPlusCircle } from "react-icons/fa";
 import './product.css'
 
-export default function ProductCheckout({ product, addItemsToCart }) {
+export default function ProductCheckout({ product, count, addItemsToCart, removeItemsFromCart }) {
     const [productInfo, setProductInfo] = useState([])
     async function fetchData() {
         try {
-            const data = await fetch(`https://localhost:44324/api/Products/${product.productId}`)
+            const data = await fetch(`https://localhost:44325/api/Products/${product.productId}`)
             setProductInfo(await data.json());
         } catch (err) {
             console.log(err)
@@ -22,6 +23,11 @@ export default function ProductCheckout({ product, addItemsToCart }) {
             <span>({productInfo.weight} g)</span>
             <span className="product-price">{productInfo.price} RON</span>
             <p className="product-description">{productInfo.description}</p>
+        </div>
+        <div className="items-count-container">
+            <FaMinusCircle size={25} color={"#49682d96"} onClick={() => removeItemsFromCart(product)} />
+            <span className="item-count">{count}</span>
+            <FaPlusCircle size={25} color={"#49682d96"} onClick={() => addItemsToCart(product)} />
         </div>
     </div >)
 }
