31301b58ae6fa6ded6c3b12264a6f0ab628cd0ce
Simonesiss <simona.radulea@gmail.com>
2021-05-12 16:39:45 +0300

    Update App & add removeItemsFromCart

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Modal from './components/Modal/Modal'
 function App() {
   const [itemsInCart, setItemsInCart] = useState([])
   const [isModalOpen, setModalOpen] = useState(false)
-  const [myOrders, setMyorders] = useState({})
+  const [myOrder, setMyOrder] = useState({})
   const [refresh, setRefresh] = useState(false)
   const [orderStatus, setOrderStatus] = useState("ok")
 
@@ -26,9 +26,27 @@ function App() {
     }
   }
 
+  const removeItemsFromCart = (item) => {
+    const itemAlreadyInCart = itemsInCart.find((it) => it.product.name === item.name)
+    if (itemAlreadyInCart) {
+      if (itemAlreadyInCart.count > 1) {
+        itemAlreadyInCart.count--;
+        setItemsInCart(itemsInCart.map((item) => {
+          if (item.product.name === item.name) {
+            return itemAlreadyInCart;
+          }
+          return item
+        }))
+      } else {
+        const productList = itemsInCart.filter(it => it.product.name !== item.name)
+        setItemsInCart(productList)
+      }
+    }
+  }
+
   async function refreshStatus(orderId) {
     try {
-      const data = await fetch(`https://localhost:44324/api/Orders/${orderId}`)
+      const data = await fetch(`https://localhost:44325/api/Orders/${orderId}`)
       const status = await data.text()
       setOrderStatus(status);
     } catch (err) {
@@ -37,8 +55,8 @@ function App() {
   }
 
   useEffect(() => {
-    if (Object.keys(myOrders).length != 0) {
-      const orderId = myOrders.orderId
+    if (Object.keys(myOrder).length != 0) {
+      const orderId = myOrder.orderId
       refreshStatus(orderId)
       let timer = setTimeout(() => setRefresh(!refresh), 5000);
       return () => {
@@ -51,11 +69,11 @@ function App() {
   return (
     <div className="App">
       <Header itemsInCart={itemsInCart} setModalOpen={setModalOpen} />
-      {isModalOpen ? <Modal itemsInCart={itemsInCart} setModalOpen={setModalOpen} setMyOrder={setMyorders} /> : ""}
-      {Object.keys(myOrders).length != 0 ? <div className="my-orders">
+      {isModalOpen ? <Modal itemsInCart={itemsInCart} setModalOpen={setModalOpen} setMyOrder={setMyOrder} addItemsToCart={addItemsToCart} removeItemsFromCart={removeItemsFromCart} /> : ""}
+      {Object.keys(myOrder).length != 0 ? <div className="my-orders">
         {/* TODO: Once you have the orders, you can list it over here, and check for it's status every 10 seconds,
         until the status is "Done" */}
-        <span>{`Status for order ${myOrders.orderId}: `}</span>
+        <span>{`Status for order ${myOrder.orderId}: `}</span>
         <span className="order-status">{orderStatus} </span>
       </div> : ""}
       <div className={`container-main ${isModalOpen ? 'blur' : ''}`}>
